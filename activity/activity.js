55599fdbc97d42334e181130e923f8790c348d7f
dawit yitayal <it.dawit.yitayal@gmail.com>
2021-03-01 03:08:44 -0800

    activity update

--- activity/activity.js
+++ activity/activity.js
@@ -2,3 +2,8 @@ class Activity{
     constructor(parsedJson){
         this.activity = parsedJson['activity']
         this.type = parsedJson['type']
+        this.participants = parsedJson['participants']
+        this.accesibility = parsedJson['accesibility']
+        
+    }
+}
