00a43dbd930a9143773c604e74ff1f0f89f1731d
dawit yitayal <it.dawit.yitayal@gmail.com>
2021-03-02 01:13:40 -0800

    maincontroller add

--- app/app.js
+++ app/app.js
@@ -5,3 +5,9 @@ const socondSlide = document.querySelector(".socondSlide");
 const thirdSlide = document.querySelector(".thirdSlide");
 const loadActivitiesButton = document.querySelector(".loadActivities");
 loadActivitiesButton.addEventListener('click', loadActivities);
+
+let activities = [];
+let mainController = async() => {
+    let services = await new Services().fetchActivities();
+    await new Services().fetchActivities();
+    await new Services().fetchActivities();
