9cba32ff0a30fbac844f309557d636ae72b50dff
dawit yitayal <it.dawit.yitayal@gmail.com>
2021-03-02 01:12:22 -0800

    import add

---
+++ app/app.js
@@ -0,0 +1,5 @@
+import {Services} from '../service/service.js';
+
+const firstSlide = document.querySelector(".firstSlide");
+const socondSlide = document.querySelector(".socondSlide");
+const thirdSlide = document.querySelector(".thirdSlide");
