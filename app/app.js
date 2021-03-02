98a631a4ceb30221bb26932f4792d52e27683d31
dawit yitayal <it.dawit.yitayal@gmail.com>
2021-03-02 01:13:01 -0800

    import updated

--- app/app.js
+++ app/app.js
@@ -3,3 +3,5 @@ import {Services} from '../service/service.js';
 const firstSlide = document.querySelector(".firstSlide");
 const socondSlide = document.querySelector(".socondSlide");
 const thirdSlide = document.querySelector(".thirdSlide");
+const loadActivitiesButton = document.querySelector(".loadActivities");
+loadActivitiesButton.addEventListener('click', loadActivities);
